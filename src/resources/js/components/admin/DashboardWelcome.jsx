c3dbfe3a40eb63aa23e7c55ddd0933a07eb2d6e4
Alex <aleksustas89@gmail.com>
2026-07-31 14:49:22 +0200

    dashboard icons to jsx

--- src/resources/js/components/admin/DashboardWelcome.jsx
+++ src/resources/js/components/admin/DashboardWelcome.jsx
@@ -1,3 +1,28 @@
+import EyeIcon from '../icons/EyeIcon';
+import OpenLinkIcon from '../icons/OpenLinkIcon';
+import CalendarIcon from '../icons/CalendarIcon';
+
+
+
 export default function DashboardWelcome({ date, name }) {
-    return <section className="shopra-welcome"><div><p>{date}</p><h1>Доброе утро, {name}! <span>👋</span></h1><small>Вот что происходит в вашем магазине сегодня.</small></div><div className="shopra-welcome-actions"><button className="shopra-button shopra-button-secondary" type="button"><span uk-icon="icon: eye; ratio: 0.8" />Посмотреть магазин<span uk-icon="icon: link-external; ratio: 0.68" /></button><button className="shopra-button shopra-button-secondary" type="button"><span uk-icon="icon: calendar; ratio: 0.8" />25 июля 2026 г.<span uk-icon="icon: chevron-down; ratio: 0.68" /></button></div></section>;
+    return (
+        <section className="shopra-welcome">
+            <div>
+                <p>{date}</p>
+                <h1>Доброе утро, {name}! <span>👋</span></h1>
+                <small>Вот что происходит в вашем магазине сегодня.</small>
+            </div>
+            <div className="shopra-welcome-actions">
+                <button className="shopra-button shopra-button-secondary" type="button">
+                    <EyeIcon/>
+                    Посмотреть магазин
+                    <OpenLinkIcon/>
+                </button>
+                <span className="shopra-button shopra-button-secondary">
+                    <CalendarIcon/>
+                    25 июля 2026 г.
+                </span>
+            </div>
+        </section>
+    );
 }
